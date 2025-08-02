386ea670a38986009e3e17a9a4a38e0ea2fddb2a
Jaime <jaimevazquezmartin@protonmail.com>
2025-08-02 18:55:06 +0200

    Add firefox to packages

--- initial_install.sh
+++ initial_install.sh
@@ -20,7 +20,7 @@ rm -rf yay
 
 # 3. Install packages using yay
 info "Installing packages with yay..."
-yay -S --noconfirm fastfetch zsh kitty syncthing obsidian nvim lazygit tree starship \
+yay -S --noconfirm fastfetch zsh kitty syncthing obsidian nvim lazygit tree starship firefox \
   zoxide fzf tmux eza htop stow ttf-firacode-nerd android-studio \
   signal-desktop android-sdk-platform-tools usbutils docker docker-compose webcord \
   fuse gnome-tweaks hyprland wofi hypridle hyprlock hyprpaper dolphin \

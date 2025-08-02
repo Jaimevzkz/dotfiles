6b5a80bb64336c18433353dd2ab683fd90a54722
Jaime <jaimevazquezmartin@protonmail.com>
2025-08-02 21:19:43 +0200

    externalize waybar wifi interface with templater

--- .config/waybar/launch_waybar.sh
+++ .config/waybar/launch_waybar.sh
@@ -1,54 +1,15 @@
 #!/bin/bash
 
-# Expand the tilde to the user's home directory
 HOME_DIR=$(eval echo ~$USER)
-WAYBAR_DIR=".config/waybar"
+WAYBAR_DIR="$HOME_DIR/.config/waybar"
 
-# Define your configurations
-CONFIG_BLUE="$HOME_DIR/$WAYBAR_DIR/blue/config"
-STYLE_BLUE="$HOME_DIR/$WAYBAR_DIR/blue/style.css"
+CONFIG_BLUE="$WAYBAR_DIR/config"
+STYLE_BLUE="$WAYBAR_DIR/style.css"
+CONFIG_TEMPLATE="$WAYBAR_DIR/config.template"
 
-CONFIG_CATTPUCCIN="$HOME_DIR/$WAYBAR_DIR/catppuccin/config"
-STYLE_CATTPUCCIN="$HOME_DIR/$WAYBAR_DIR/catppuccin/style.css"
+# Run envsubst on the full path to create config
+envsubst < "$CONFIG_TEMPLATE" > "$CONFIG_BLUE"
 
-CONFIG_TYPECRAFT="$HOME_DIR/$WAYBAR_DIR/typecraft/config.jsonc"
-STYLE_TYPECRAFT="$HOME_DIR/$WAYBAR_DIR/typecraft/style.css"
-
-CONFIG_MINIM_BLUE="$HOME_DIR/$WAYBAR_DIR/minimalistic_blue/config.jsonc"
-STYLE_MINIM_BLUE="$HOME_DIR/$WAYBAR_DIR/minimalistic_blue/style.css"
-
-# Check if a config number is passed as an argument
-if [ -z "$1" ]; then
-    echo "Usage: $0 <config_number (1=Blue, 2=Catppuccin, 3=Typecraft, 4=Minimalistic blue)>"
-    exit 1
-fi
-
-CONFIG_NUMBER="$1"
-
-# Select the configuration based on the number
-case $CONFIG_NUMBER in
-    1)
-        WAYBAR_CONFIG=$CONFIG_BLUE
-        WAYBAR_STYLE=$STYLE_BLUE
-        ;;
-    2)
-        WAYBAR_CONFIG=$CONFIG_CATTPUCCIN
-        WAYBAR_STYLE=$STYLE_CATTPUCCIN
-        ;;
-    3)
-        WAYBAR_CONFIG=$CONFIG_TYPECRAFT
-        WAYBAR_STYLE=$STYLE_TYPECRAFT
-        ;;
-    4)
-        WAYBAR_CONFIG=$CONFIG_MINIM_BLUE
-        WAYBAR_STYLE=$STYLE_MINIM_BLUE
-        ;;
-    *)
-        echo "Invalid configuration number. Please choose 1, 2, or 3."
-        exit 1
-        ;;
-esac
-
-# Launch Waybar with custom config and style
-waybar -c "$WAYBAR_CONFIG" -s "$WAYBAR_STYLE" & disown
+# Now run waybar with absolute config and style paths
+waybar -c "$CONFIG_BLUE" -s "$STYLE_BLUE" & disown
 

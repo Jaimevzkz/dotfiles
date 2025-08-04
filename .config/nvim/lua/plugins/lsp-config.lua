425f0fcdf88116bd3507f3b42fa63d8df2e576e9
Jaime <jaimevazquezmartin@protonmail.com>
2025-08-04 08:55:11 +0200

    Update lsp config file

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -9,7 +9,9 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "kotlin_language_server", "rust_analyzer", "gopls" },
+        --ensure_installed = { "lua_ls", "kotlin_language_server", "rust_analyzer", "gopls" },
+        automatic_enable = true,
+
       })
     end,
   },
@@ -28,9 +30,9 @@ return {
       lspconfig.rust_analyzer.setup({
         capabilities = capabilities,
       })
-      lspconfig.gopls.setup({
-        capabilities = capabilities,
-      })
+      --      lspconfig.gopls.setup({
+      --       capabilities = capabilities,
+      --    })
 
       -- LSP commands
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})

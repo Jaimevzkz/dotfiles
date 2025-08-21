eef8bdfec0b45d67fede2e03e6aa8e9a08e6a5a3
Jaime <jaimevazquezmartin@protonmail.com>
2025-08-21 11:56:10 +0200

    add log highlighting plugin

---
+++ .config/nvim/lua/plugins/log-highlighting.lua
@@ -0,0 +1,28 @@
+return {
+  'fei6409/log-highlight.nvim',
+  opts = {},
+  config = function ()
+    require('log-highlight').setup {
+      ---@type string|string[]  File extensions. Default: 'log'
+      extension = {
+        'log',
+        'txt'
+      },
+
+      ---@type string|string[]  File names or full file paths. Default: {}
+      filename = {
+        -- log.txt  
+      },
+
+      ---@type string|string[]  File name/path glob patterns. Default: {}
+      pattern = {
+        -- Use `%` to escape special characters and match them literally.
+--        '%/var%/log%/.*',
+--       'console%-ramoops.*',
+--       'log.*%.txt',
+--        'logcat.*',
+      },
+    }
+
+  end
+}

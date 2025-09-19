c4ba8044647c96528aa5655807cb9b5f5ddb2ae1
Jaime <jaimevazquezmartin@protonmail.com>
2025-09-19 12:53:38 +0200

    Fix nvim config

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -9,33 +9,48 @@ return {
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup({
-				--ensure_installed = { "lua_ls", "kotlin_language_server", "rust_analyzer", "gopls" },
-				automatic_enable = true,
+				-- ensure_installed = { "lua_ls", "kotlin_language_server", "rust_analyzer", "gopls" },
+				automatic_installation = true,
 			})
 		end,
 	},
 	{
 		"neovim/nvim-lspconfig",
 		config = function()
-			local lspconfig = require("lspconfig")
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			lspconfig.lua_ls.setup({
+			-- Define configs
+			vim.lsp.config("lua_ls", {
 				capabilities = capabilities,
 			})
-			lspconfig.kotlin_language_server.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.rust_analyzer.setup({
-				capabilities = capabilities,
-			})
-			--      lspconfig.json_lsp.setup({
-			--       capabilities = capabilities,
-			--     })
 
-			-- LSP commands
+			vim.lsp.config("kotlin_language_server", {
+				capabilities = capabilities,
+			})
+
+			vim.lsp.config("rust_analyzer", {
+				capabilities = capabilities,
+			})
+
+			-- Automatically start attached servers for opened buffers
+			vim.api.nvim_create_autocmd("FileType", {
+				callback = function(args)
+					local ft = args.match
+					local clients = {
+						lua = "lua_ls",
+						kotlin = "kotlin_language_server",
+						rust = "rust_analyzer",
+					}
+					local server = clients[ft]
+					if server then
+						vim.lsp.start(vim.lsp.config(server))
+					end
+				end,
+			})
+
+			-- LSP keymaps
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {}) --Use ^o to nav back
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
 		end,
 	},

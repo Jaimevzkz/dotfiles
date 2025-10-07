95079907c986c06c55afc8a7a7672ab528504558
Jaime <jaimevazquezmartin@protonmail.com>
2025-10-07 15:23:13 +0200

    Update mediaplayer script

--- .config/waybar/mediaplayer.py
+++ .config/waybar/mediaplayer.py
@@ -127,9 +127,9 @@ class PlayerManager:
 
         if track_info:
             if player.props.status == "Playing":
-                track_info = " " + track_info
-            else:
                 track_info = " " + track_info
+            else:
+                track_info = " " + track_info
 
         current_playing = self.get_first_playing_player()
         if current_playing is None or current_playing.props.player_name == player.props.player_name:

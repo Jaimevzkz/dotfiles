80d55a6b253af7e51090b7d53dceeef560fbf410
Jaime <jaimevazquezmartin@protonmail.com>
2025-10-23 17:59:15 +0200

    Update Config

--- server_initial_install.sh
+++ server_initial_install.sh
@@ -22,7 +22,8 @@ rm -rf yay
 info "Installing packages with yay..."
 yay -S --noconfirm fastfetch zsh nvim tree starship lazygit \
   zoxide wget fzf tmux eza htop stow ttf-firacode-nerd bat\
-   otf-font-awesome ttf-space-mono-nerd bind\
+   otf-font-awesome ttf-space-mono-nerd bind \
+ docker docker-compose \
 
 # Install Oh My Zsh if not already installed
 if [ ! -d "${ZSH:-$HOME/.oh-my-zsh}" ]; then

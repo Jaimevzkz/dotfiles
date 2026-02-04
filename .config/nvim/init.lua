038b30f1217aad00fd24b8ededa21d07fcf1a8b3
Jaime <jaimevazquezmartin@protonmail.com>
2026-02-04 16:08:03 +0100

    Update Config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -30,7 +30,7 @@ vim.keymap.set("n", "<leader>v", ":wincmd v<CR>")
 vim.keymap.set("n", "<leader>s", ":wincmd s<CR>")
 
 -- REMAPS
-vim.keymap.set("n", "<leader>h", ":nohlsearch <CR>") -- Quit highlighting
+vim.keymap.set("n", "<leader>hq", ":nohlsearch <CR>") -- Quit highlighting
 
 vim.keymap.set("n", "<leader>o", "o<Esc>k")
 vim.keymap.set("n", "<leader>O", "O<Esc>j")
--- .config/nvim/lua/plugins/harpoon.lua
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -16,14 +16,11 @@ return {
 			harpoon.ui:toggle_quick_menu(harpoon:list())
 		end)
 
-		vim.keymap.set("n", "<C-u>", function()
+		vim.keymap.set("n", "<C-i>", function()
 			harpoon:list():select(1)
 		end)
-		vim.keymap.set("n", "<C-i>", function()
-			harpoon:list():select(2)
-		end)
 		vim.keymap.set("n", "<C-o>", function()
-			harpoon:list():select(3)
+			harpoon:list():select(2)
 		end)
 
 		-- Toggle previous & next buffers stored within Harpoon list
